aa7889c8d0babdfcaa5c3cc86171c3f36f4218d2
Leothi <leohb2@gmail.com>
2020-01-24 13:17:03 -0200

    Extracting some reviews, started cleaning.

--- scraper_bb.py
+++ scraper_bb.py
@@ -7,47 +7,27 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from config import *
 
-options = webdriver.FirefoxOptions()
-options.headless = True
+from utils_text import strip_accents, preprocess_text
 
-profile = webdriver.FirefoxProfile()
+chrome_options = Options()
+chrome_options.add_argument('headless')
 
-# PDF Download
-profile.set_preference("browser.download.folderList", 2)
-profile.set_preference("browser.download.manager"
-                       ".showWhenStarting", False)
-profile.set_preference("browser.download.dir", DOWNLOADS_PATH)
-profile.set_preference("browser.helperApps.neverAsk.saveToDisk",
-                       "application/pdf")
+NUM_REVIEWS = 10
 
-profile.set_preference("pdfjs.disabled", True)
-profile.set_preference("plugin.scan.Acrobat", "99.0")
-profile.set_preference("plugin.scan.plid.all", False)
-
-profile.set_preference("network.proxy.type", 1)
-profile.set_preference("network.proxy.http", PROXY_HOST)
-profile.set_preference("network.proxy.http_port", PROXY_PORT)
-profile.set_preference("network.proxy.socks_username", PROXY_USER)
-profile.set_preference("network.proxy.socks_password", PROXY_PASS)
-
-with webdriver.Firefox(executable_path=SELENIUM_DRIVER,
-                       firefox_profile=profile, options=options) as driver:
+with webdriver.Chrome(options=chrome_options) as driver:
     wait = WebDriverWait(driver, 5)
 
+    link = "https://play.google.com/store/apps/details?id=br.com.bb.android&hl=pt&showAllReviews=true"
     all_reviews_xpath = '//*[@id="fcxH9b"]/div[4]/c-wiz/div/div[2]/div/div[1]/div/div/div[1]/div[2]/div'
     name_xpath = '//*[@id = "fcxH9b"]/div[4]/c-wiz/div/div[2]/div/div[1]/div/div/div[1]/div[2]/div/div[1]/div/div[2]/div[1]/div[1]/span'
 
-    driver.get(
-        "https://play.google.com/store/apps/details?id=br.com.bb.android&hl=pt&showAllReviews=true")
+    driver.get(link)
 
-    reviews = driver.find_element_by_xpath(all_reviews_xpath)
-    names = reviews.find_element_by_xpath(name_xpath)
+    for i in range(NUM_REVIEWS):
+        logger.info(f'REVIEW #{i+1}')
+        current_xpath = f'/html/body/div[1]/div[4]/c-wiz/div/div[2]/div/div[1]/div/div/div[1]/div[2]/div/div[{i+1}]'
+        current_review = driver.find_element_by_xpath(current_xpath)
 
-    for name in names:
-        print(name.text)
-    # sleep(5)
-    # extracted_text = review.text
-
-# print(extracted_text)
+        clean_text = strip_accents(current_review.text)
+        print(clean_text)
