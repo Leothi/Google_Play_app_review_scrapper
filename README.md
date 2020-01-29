13d1bb4e44f0ef61f853913ac5b05dfd3784587f
Leothi <leohb2@gmail.com>
2020-01-29 17:23:18 -0200

    working with 1000 reviews, generating csv file

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Google_Play_app_review_scrapper
-Scraps n reviews from a Google Play app.
+
+Scrap reviews from Google Play website and generate a csv file with them
+
+## Getting Started
+
+How to run the project.
+
+### Prerequisites
+
+* All requirements installed;
+* Firefox geckodriver on path.
+
+### Installing
+
+* Clone the repository;
+* Install requirements with `pip3 install -r requirements.txt`
+
+### How to use
+* Use `python3 scrapper.py arg1 arg2`, where arg1 is the full app reviews website and arg2 the number of reviews to scrap.
+* Example:
+
+`python3 https://play.google.com/store/apps/details?id=com.sagosago.Babies.googleplay&hl=pt&showAllReviews=true 500`
+
+## Authors
+
+* **Leonardo Bonifácio** - *AI Developer*
+
+## Acknowledgments
+
+* Any help or tip, feel free to contact me.
--- scrapper.py
+++ scrapper.py
@@ -16,12 +16,14 @@ from utils_text import strip_accents, preprocess_text
 chrome_options = Options()
 chrome_options.add_argument('headless')
 
+excep_counter = 0
+
 
 def scrapper(link: str, num_reviews: int, verbose: bool = False):
     reviews_dict = defaultdict(list)
 
     with webdriver.Chrome(options=chrome_options) as driver:
-        wait = WebDriverWait(driver, 5)
+        wait = WebDriverWait(driver, 3)
 
         logger.info(f'[+] Initializing Scrapper with {num_reviews} reviews')
         try:
@@ -30,30 +32,37 @@ def scrapper(link: str, num_reviews: int, verbose: bool = False):
             logger.warning("Can't get website")
 
         for i in range(num_reviews):
+            excep_name = False
+            excep_date = False
+            excep_description = False
+
+            # Getting name
             try:
-                # Getting name
                 current_name_xpath = f'/html/body/div[1]/div[4]/c-wiz/div/div[2]/div/div[1]/div/div/div[1]/div[2]/div[1]/div[{i+1}]/div/div[2]/div[1]/div[1]/span'
                 wait.until(EC.presence_of_element_located(
                     (By.XPATH, current_name_xpath)))
                 current_name = driver.find_element_by_xpath(current_name_xpath)
             except TimeoutException:
+                excep_name = True
                 logger.warning("Can't get name")
 
+            # Getting date
             try:
-                # Getting date
                 current_date_xpath = f'/html/body/div[1]/div[4]/c-wiz/div/div[2]/div/div[1]/div/div/div[1]/div[2]/div[1]/div[{i+1}]/div/div[2]/div[1]/div[1]/div/span[2]'
                 current_date = driver.find_element_by_xpath(current_date_xpath)
             except NoSuchElementException:
+                excep_date = True
                 logger.warning("Can't get date")
 
+            # Getting description
             try:
-                # Getting description
                 current_description_xpath = f'/html/body/div[1]/div[4]/c-wiz/div/div[2]/div/div[1]/div/div/div[1]/div[2]/div[1]/div[{i+1}]/div/div[2]/div[2]/span[1]'
                 current_description = driver.find_element_by_xpath(
                     current_description_xpath)
 
                 # If description is not complete, need to click "show more" button
-                if 'completa' in current_description.text.split()[-1] or 'review' in current_description.text.split()[-1]:
+                desc_last_word = current_description.text.split()[-1]
+                if 'completa' in desc_last_word or 'review' in desc_last_word:
                     current_button_xpath = f'/html/body/div[1]/div[4]/c-wiz/div/div[2]/div/div[1]/div/div/div[1]/div[2]/div/div[{i+1}]/div/div[2]/div[2]/span[1]/div/button'
                     wait.until(EC.element_to_be_clickable(
                         (By.XPATH, current_button_xpath)))
@@ -62,36 +71,49 @@ def scrapper(link: str, num_reviews: int, verbose: bool = False):
                     driver.execute_script(
                         "arguments[0].click();", current_button)
 
+                    # Getting full description
                     current_description_xpath = f'/html/body/div[1]/div[4]/c-wiz/div/div[2]/div/div[1]/div/div/div[1]/div[2]/div[1]/div[{i+1}]/div/div[2]/div[2]/span[2]'
                     current_description = driver.find_element_by_xpath(
                         current_description_xpath)
             except (TimeoutException, NoSuchElementException):
+                excep_description = True
                 logger.warning("Can't get description")
 
-            # Scrolling down pages to get more reviews
+            # Scrolling down page to get more reviews
             driver.execute_script(
                 "window.scrollTo(0, document.body.scrollHeight);")
-            try:
-                if verbose:
-                    print(current_name.text)
-                    print(current_date.text)
-                    print(strip_accents(current_description.text))
 
-                # Getting all reviews in a dict
+            # Clicking show more button every 100 reviews to get more reviews
+            if (i % 100 == 0 and i != 0):
+                try:
+                    show_more_button = driver.find_element_by_xpath(
+                        "//*[contains(text(), 'Mostrar mais')]")
+                    driver.execute_script(
+                        "arguments[0].click();", show_more_button)
+                except NoSuchElementException:
+                    print('excep')
+
+            # Putting all reviews in a dict
+            if not any([excep_name, excep_date, excep_description]):
                 pre_processed_text = preprocess_text(current_name.text)
                 reviews_dict['name'].append(pre_processed_text)
-
-                reviews_dict['date'].append(current_date.text)
 
                 pre_processed_description = preprocess_text(
                     current_description.text)
                 reviews_dict['review'].append(pre_processed_description)
 
-                logger.info(f'Review #{i+1} DONE')
-            except UnboundLocalError:
-                logger.warning(f'Review #{i+1} NOT DONE')
+                reviews_dict['date'].append(current_date.text)
 
-        logger.success('[+] Scrapping DONE')
+                excep_counter = 0
+                logger.info(f'Review #{i+1} DONE')
+            else:
+                excep_counter += 1
+                logger.warning(
+                    f'Review #{i+1} NOT DONE, terminating in {3-excep_counter}')
+
+                if excep_counter >= 3:
+                    logger.success('[+] Scrapping DONE')
+                    break
 
         return reviews_dict
 
@@ -100,6 +122,7 @@ def save_reviews(all_reviews: dict, output_file: str = 'scrapper_results.csv'):
     if all_reviews:
         df = pd.DataFrame(all_reviews)
         df.to_csv(output_file, ';', index=False)
+        logger.success('[+] CSV file created')
     else:
         logger.warning('All_reviews dict empty')
 
