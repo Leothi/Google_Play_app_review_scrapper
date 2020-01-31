021451cd8ca14a696532a29a4f7baac1e5b22892
Leothi <leohb2@gmail.com>
2020-01-31 17:39:50 -0200

    added stars, likes, random file name

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import random
+import re
 
 from sys import argv
 from loguru import logger
@@ -13,8 +15,11 @@ from selenium.common.exceptions import TimeoutException, WebDriverException, NoS
 
 chrome_options = Options()
 chrome_options.add_argument('headless')
+logger.add("scrapper.log")
 
 excep_counter = 0
+random_number_name = random.randint(0, 1000)
+RE_STARS = re.compile(r'(?:Avaliado com )(\d)')
 
 
 def scrapper(link: str, num_reviews: int, verbose: bool = False):
@@ -33,6 +38,8 @@ def scrapper(link: str, num_reviews: int, verbose: bool = False):
             excep_name = False
             excep_date = False
             excep_description = False
+            excep_likes = False
+            excep_stars = False
 
             # Getting name
             try:
@@ -43,6 +50,31 @@ def scrapper(link: str, num_reviews: int, verbose: bool = False):
             except TimeoutException:
                 excep_name = True
                 logger.warning("Can't get name")
+
+            # Getting stars
+            try:
+                stars_xpath = f'/html/body/div[1]/div[4]/c-wiz/div/div[2]/div/div[1]/div/div/div[1]/div[2]/div/div[{i+1}]/div/div[2]/div[1]/div[1]/div/span[1]/div/div'
+                wait.until(EC.presence_of_element_located(
+                    (By.XPATH, stars_xpath)))
+                current_stars = driver.find_element_by_xpath(stars_xpath)
+                current_stars_text = current_stars.get_attribute('aria-label')
+                current_stars_text = RE_STARS.findall(current_stars_text)[0]
+            except TimeoutException:
+                excep_stars = True
+                logger.warning("Can't get stars")
+
+            # Getting likes
+            try:
+                likes_xpath = f'/html/body/div[1]/div[4]/c-wiz/div/div[2]/div/div[1]/div/div/div[1]/div[2]/div/div[{i+1}]/div/div[2]/div[1]/div[2]/div/div[1]/div[2]'
+                wait.until(EC.presence_of_element_located(
+                    (By.XPATH, likes_xpath)))
+                current_likes = driver.find_element_by_xpath(likes_xpath)
+                current_likes_text = current_likes.text
+                if not current_likes_text:
+                    current_likes_text = '0'
+            except TimeoutException:
+                excep_likes = True
+                logger.warning("Can't get likes")
 
             # Getting date
             try:
@@ -73,7 +105,7 @@ def scrapper(link: str, num_reviews: int, verbose: bool = False):
                     current_description_xpath = f'/html/body/div[1]/div[4]/c-wiz/div/div[2]/div/div[1]/div/div/div[1]/div[2]/div[1]/div[{i+1}]/div/div[2]/div[2]/span[2]'
                     current_description = driver.find_element_by_xpath(
                         current_description_xpath)
-            except (TimeoutException, NoSuchElementException):
+            except (TimeoutException, NoSuchElementException, WebDriverException):
                 excep_description = True
                 logger.warning("Can't get description")
 
@@ -92,12 +124,14 @@ def scrapper(link: str, num_reviews: int, verbose: bool = False):
                     pass
 
             # Putting all reviews in a dict
-            if not any([excep_name, excep_date, excep_description]):
+            if not any([excep_name, excep_date, excep_description, excep_likes, excep_stars]):
                 excep_counter = 0
 
                 reviews_dict['name'].append(current_name.text)
                 reviews_dict['raw_review'].append(current_description.text)
                 reviews_dict['date'].append(current_date.text)
+                reviews_dict['likes'].append(current_likes_text)
+                reviews_dict['stars'].append(current_stars_text)
 
                 logger.info(f'Review #{i+1} DONE')
             else:
@@ -112,11 +146,11 @@ def scrapper(link: str, num_reviews: int, verbose: bool = False):
         return reviews_dict
 
 
-def save_reviews(all_reviews: dict, output_file: str = 'scrapper_results.csv'):
+def save_reviews(all_reviews: dict, output_file: str = f'scrapper_results_{random_number_name}.csv'):
     if all_reviews:
         df = pd.DataFrame(all_reviews)
         df.to_csv(output_file, index=False)
-        logger.success('[+] CSV file created')
+        logger.success(f'[+] CSV file created. Filename: {output_file}')
     else:
         logger.warning('All_reviews dict empty')
 
